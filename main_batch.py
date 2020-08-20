1ebc446d8c16ebd317caa31ac033ee15b38c09b6
Tomer Cohen <tomer.cohen13@cs.huji.ac.il>
2020-08-20 13:33:53 +0300

    Update main_batch.py

--- main_batch.py
+++ main_batch.py
@@ -3,20 +3,29 @@ import sys
 import os
 import subprocess
 
-PWD = "/cs/labs/dina/tomer.cohen13/NR_H_Protein_Martin_1000/"
+# max memory for each batch
+MEMORY = "4000m"
+
+# max time for each batch
+TIME = "24:0:0"
+
+# the begining of the script for cluster
 INTRO = "#!/bin/tcsh\n" \
-        "#SBATCH --mem=4000m\n" \
+        "#SBATCH --mem="+ MEMORY +"\n" \
         "#SBATCH -c1\n" \
-        "#SBATCH --time=24:0:0\n" \
+        "#SBATCH --time=" + TIME + "\n" \
         "cd /cs/labs/dina/tomer.cohen13\n" \
-        "python3 /cs/usr/tomer.cohen13/Desktop/nanobodies/nanobodies_script.py " + PWD
+        "python3 /cs/usr/tomer.cohen13/Desktop/nanobodies/nanobodies_script.py "
 
 
 if __name__ == '__main__':
-    for file in os.listdir(sys.argv[1]):
-        if file.endswith('.pdb'):
-            script_name = PWD + file + ".sh"
+    # make sure that you go to cluster (like hm) before calling this program!
+
+    pwd = sys.argv[1]  # working directory
+    for file in os.listdir(pwd):
+        if file.endswith('.pdb'):  # goes over all pdb files in that directory
+            script_name = pwd + "/" + file + ".sh"  # script file
             with open(script_name, 'w') as f:
-                line = INTRO + file.split(".")[0] + " " + file
+                line = INTRO + pwd + "/" + file.split(".")[0] + " " + file 
                 f.write(line)
-            subprocess.run("sbatch " + script_name, shell=True)
+            subprocess.run("sbatch " + script_name, shell=True)  # sends script to the cluster
