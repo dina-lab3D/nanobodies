ae670f14afe360d18bf538c30fe7d8c2b2f9d87e
tomer.cohen13 <tomer.cohen13@mail.huji.ac.il>
2020-08-25 18:45:35 +0300

    - changed the SCRIPT_PATH - changed the creation of the pdb folder (so the slurm files will appear in the right folder)

--- nano_buddies/main_batch.py
+++ nano_buddies/main_batch.py
@@ -7,15 +7,17 @@ import subprocess
 MEMORY = "8000m"
 
 # max time for each batch
-TIME = "48:0:0"
+TIME = "8:0:0"
+
+SCRIPT_PATH = "/cs/labs/dina/tomer.cohen13/nanobodies/nano_buddies/nanobodies_script.py"
 
 # the begining of the script for cluster
 INTRO = "#!/bin/tcsh\n" \
         "#SBATCH --mem="+ MEMORY +"\n" \
         "#SBATCH -c1\n" \
-        "#SBATCH --time=" + TIME + "\n" \
-        "cd /cs/labs/dina/tomer.cohen13\n" \
-        "python3 /cs/usr/tomer.cohen13/Desktop/nanobodies/nanobodies_script.py "
+        "#SBATCH --time=" + TIME + "\n"
+
+
 
 
 if __name__ == '__main__':
@@ -23,13 +25,20 @@ if __name__ == '__main__':
 
     pwd = sys.argv[1]  # working directory
     for file in os.listdir(pwd):
-        
         if file.endswith('.pdb'):  # goes over all pdb files in that directory
             current_folder_path = pwd + "/" + file.split(".")[0]
-            script_name = pwd + "/" + file + ".sh"  # script file
+            print(current_folder_path)
+
+            if not os.path.isdir(current_folder_path):
+                os.mkdir(current_folder_path)
+
+            os.chdir(current_folder_path)
+            script_name = pwd + "/" + file.split(".")[0] + "/" + file + ".sh"  # script file
             with open(script_name, 'w') as f:
-                line = INTRO + pwd + "/" + file.split(".")[0] + " " + file
-                
+                #line = INTRO + pwd + "/" + file.split(".")[0] + " " + file
+                f.write(INTRO)
+                line = "cd " + current_folder_path + "\n"
                 f.write(line)
-         
+                line = "python3 " + SCRIPT_PATH + " " + current_folder_path + " " + file
+                f.write(line)
             subprocess.run("sbatch " + script_name, shell=True)  # sends script to the cluster
