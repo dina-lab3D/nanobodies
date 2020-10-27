0b4360e662ef4110220e628003f8f53088c2b25d
tomer.cohen13 <tomer.cohen13@mail.huji.ac.il>
2020-10-27 13:41:35 +0200

    - fixed bug of failed docking

--- COVID_19/InterfaceMaker.py
+++ COVID_19/InterfaceMaker.py
@@ -10,11 +10,13 @@ HEADER = "/cs/usr/tomer.cohen13/lab/nanobodies/COVID_19/header.csv"
 
 def get_interface(folder):
     os.chdir(folder)
-    subprocess.run(INTERFACE + " ABC H 6 nanobody_trans_*.pdb", shell=True)
-    df = pd.read_csv("epi.csv", header=None)
+    print(folder)
+    df = None
+    if os.path.exists("nanobody_trans_0.pdb"):
+        subprocess.run(INTERFACE + " ABC H 6 nanobody_trans_*.pdb", shell=True)
+        df = pd.read_csv("epi.csv", header=None)
     os.chdir("..")
     return df
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -30,5 +32,6 @@ if __name__ == '__main__':
             #  if the folder is pdb folder
             if os.path.isdir(directory) and re.fullmatch("[a-zA-Z0-9]{4}_[0-9]", directory):
                 epi_df = get_interface(directory)
-                epi_df.insert(0, column="PDB", value=directory)
-                epi_df.to_csv(epi_file, header=False, index=False)
+                if epi_df is not None:
+                    epi_df.insert(0, column="PDB", value=directory)
+                    epi_df.to_csv(epi_file, header=False, index=False)
