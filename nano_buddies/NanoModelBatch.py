802668419fe6b0159d550aeb9127b8ad383d1b14
tomer.cohen13 <tomer.cohen13@mail.huji.ac.il>
2021-01-16 14:40:17 +0200

    rmsd calc for rosetta results

--- nano_buddies/NanoModelBatch.py
+++ nano_buddies/NanoModelBatch.py
@@ -4,19 +4,10 @@ import os
 import subprocess
 
 # 'loop_restraints'/'model_nanobody'/'rosetta_model'/'rosetta_loops'
-MODE = 'rosetta_loops'
-
-# max memory for each batch
-MEMORY = "4500m"
-
-# max time for each batch
-TIME = "5-0"
+MODE = 'rosetta_model'
 
 # NanoModelScript.py path
 SCRIPT_PATH = "/cs/labs/dina/tomer.cohen13/nanobodies/nano_buddies/NanoModelScript.py"
-
-# -t for testing mode, -l <int> for number of loops to model
-FLAGS = "-t -l 100"
 
 # LoopRestraints.py path
 RESTRAINTS_PATH = "/cs/labs/dina/tomer.cohen13/nanobodies/nano_buddies/LoopRestraints.py"
@@ -29,10 +20,130 @@ FAILED = ["1YC7_1"]
 
 # the begining of the script for cluster
 INTRO = "#!/bin/tcsh\n" \
-        "#SBATCH --mem="+ MEMORY +"\n" \
+        "#SBATCH --mem={}\n" \
         "#SBATCH -c2\n" \
-        "#SBATCH --time=" + TIME + "\n" \
-        "#SBATCH --array=1-3\n"
+        "#SBATCH --time={}\n" #\
+        # "#SBATCH --array=1-3\n"
+
+
+def model_nanobody():
+    """
+
+    :return:
+    """
+    time, memory = "8:0:0", "7000m"
+    # -t for testing mode, -l <int> for number of loops to model
+    flags = "-t -l 100"
+    for file in os.listdir(os.getcwd()):
+        if file.endswith('.pdb'):  # goes over all pdb files in that directory
+            folder_name = file.split(".")[0]  # the new pdb folder
+            if not os.path.isdir(folder_name):
+                os.mkdir(folder_name)
+
+            os.chdir(folder_name)
+            script_name = folder_name + ".sh"  # script file
+
+            with open(script_name, 'w') as f:
+                f.write(INTRO.format(memory, time))
+                f.write("cd " + os.getcwd() + "\n")
+
+                # get chain H (antibody)
+                f.write("~dina/utils/getChain.Linux H " + os.getcwd() + ".pdb" + " > " + os.getcwd() + "/ref.pdb")
+
+                # fasta script
+                f.write("~dina/utils/pdb2fasta " + os.getcwd() + "/ref.pdb" + " > " + os.getcwd() + "/" + folder_name + ".fa")
+
+                # run the script that creates the loops models
+                f.write("~dina/modeller9.18/bin/modpy.sh python3 /cs/labs/dina/tomer.cohen13/nanobodies/scripts/modelNanobody.py " + flags + " " + os.getcwd() +"/" + folder_name + ".fa")
+
+                # move the pdb file
+                f.write("mv " + os.getcwd() + ".pdb " + os.getcwd())
+            subprocess.run("sbatch " + script_name,shell=True)  # sends script to the cluster
+
+            os.chdir("..")
+
+
+def loop_restraints():
+    """
+
+    :return:
+    """
+    time, memory = "14:0:0", "7000m"
+
+    for pdb_dir in os.listdir(os.getcwd()):
+        os.chdir(pdb_dir)
+        script_name = pdb_dir + ".sh"  # script file
+        with open(script_name, 'w') as f:
+            f.write(INTRO.format(memory, time))
+            f.write("cd " + os.getcwd() + "\n")
+            f.write("module load tensorflow/2.0.0" + "\n")
+            f.write("source  ~/venv/plot/bin/activate.csh" + "\n")
+            f.write("~dina/modeller9.18/bin/modpy.sh python3 " + RESTRAINTS_PATH + " " + os.getcwd() + " " + NANO_NET_MODEL_PATH)
+        subprocess.run("sbatch " + script_name,shell=True)  # sends script to the cluster
+        os.chdir("..")
+
+
+def rosetta_model():
+    """
+
+    :return:
+    """
+    time, memory = "6:0:0", "7000m"
+
+    for pdb_dir in os.listdir(os.getcwd()):
+        if pdb_dir not in FAILED:
+            continue
+            # -detect_disulf false
+            # -camelid true
+        os.chdir(pdb_dir)
+        script_name = pdb_dir + ".sh"  # script file
+        with open(script_name, 'w') as f:
+            f.write(INTRO.format(memory, time))
+            f.write("cd " + os.getcwd() + "\n")
+            f.write("setenv ROSETTA /cs/labs/dina/tomer.cohen13/Rosetta\n")
+            f.write("setenv ROSETTA3_DB $ROSETTA/main/database\n")
+            f.write("setenv ROSETTA_BIN $ROSETTA/main/source/bin\n")
+            f.write("setenv PATH $PATH':'$ROSETTA_BIN\n")
+            f.write("setenv PATH $PATH':'/cs/labs/dina/tomer.cohen13/Blast/bin\n")
+            f.write("antibody.linuxgccrelease -exclude_homologs true -vhh_only -fasta " + pdb_dir + ".fa | tee grafting.log\n")
+            f.write("cd grafting\n")
+            f.write("rm -f debug*\n")
+            f.write("rm -f orientation*\n")
+            f.write("rm -f frh*.pdb*\n")
+            f.write("rm -f frl*\n")
+            f.write("rm -f l*\n")
+        subprocess.run("sbatch " + script_name,shell=True)  # sends script to the cluster
+        os.chdir("..")
+
+
+def rosetta_loops():
+    """
+
+    :return:
+    """
+    time, memory, array = "5-0", "4500m", "3"
+
+    for pdb_dir in os.listdir(os.getcwd()):
+        if pdb_dir in FAILED:
+            continue
+            # -detect_disulf false
+            # -camelid true
+        os.chdir(pdb_dir)
+        script_name = pdb_dir + ".sh"  # script file
+        with open(script_name, 'w') as f:
+            f.write(INTRO.format(memory, time))
+            f.write("SBATCH --array=1-{}\n".format(array))
+            f.write("cd " + os.getcwd() + "\n")
+            f.write("setenv ROSETTA /cs/labs/dina/tomer.cohen13/Rosetta\n")
+            f.write("setenv ROSETTA3_DB $ROSETTA/main/database\n")
+            f.write("setenv ROSETTA_BIN $ROSETTA/main/source/bin\n")
+            f.write("setenv PATH $PATH':'$ROSETTA_BIN\n")
+            f.write("setenv PATH $PATH':'/cs/labs/dina/tomer.cohen13/Blast/bin\n")
+            f.write("antibody_H3.linuxgccrelease @abH3.flags -s grafting/model-0.relaxed.pdb -nstruct 200 -out:file:scorefile H3_modeling_scores.fasc -out:path:pdb H3_modeling > h3_modeling-0.log\n")
+            # for model in range(1,10):
+            #     f.write("antibody_H3.linuxgccrelease @abH3.flags -s grafting/model-{}.relaxed.pdb -nstruct 100 > h3_modeling-{}.log\n".format(model, model))
+        subprocess.run("sbatch " + script_name,shell=True)  # sends script to the cluster
+        os.chdir("..")
 
 
 if __name__ == '__main__':
@@ -41,100 +152,20 @@ if __name__ == '__main__':
     pwd = sys.argv[1]  # working directory
     os.chdir(pwd)
 
-
     # for moidelNanobody script
     if MODE == "model_nanobody":
-        for file in os.listdir(os.getcwd()):
-            if file.endswith('.pdb'):  # goes over all pdb files in that directory
-                folder_name = file.split(".")[0]  # the new pdb folder
-                if not os.path.isdir(folder_name):
-                    os.mkdir(folder_name)
-
-                os.chdir(folder_name)
-                script_name = folder_name + ".sh"  # script file
-
-                with open(script_name, 'w') as f:
-                    f.write(INTRO)
-                    f.write("cd " + os.getcwd() + "\n")
-
-                    # get chain H (antibody)
-                    f.write("~dina/utils/getChain.Linux H " + os.getcwd() + ".pdb" + " > " + os.getcwd() + "/ref.pdb")
-
-                    # fasta script
-                    f.write("~dina/utils/pdb2fasta " + os.getcwd() + "/ref.pdb" + " > " + os.getcwd() + "/" + folder_name + ".fa")
-
-                    # run the script that creates the loops models
-                    f.write("~dina/modeller9.18/bin/modpy.sh python3 /cs/labs/dina/tomer.cohen13/nanobodies/scripts/modelNanobody.py " + FLAGS + " " + os.getcwd() +
-                            "/" + folder_name + ".fa")
-
-                    # move the pdb file
-                    f.write("mv " + os.getcwd() + ".pdb " + os.getcwd())
-                subprocess.run("sbatch " + script_name, shell=True)  # sends script to the cluster
-
-                os.chdir("..")
-
+        model_nanobody()
 
     # for LoopRestraints script
     if MODE == "loop_restraints":
-        for pdb_dir in os.listdir(os.getcwd()):
-            os.chdir(pdb_dir)
-            script_name = pdb_dir + ".sh"  # script file
-            with open(script_name, 'w') as f:
-                f.write(INTRO)
-                f.write("cd " + os.getcwd() + "\n")
-                f.write("module load tensorflow/2.0.0" + "\n")
-                f.write("source  ~/venv/plot/bin/activate.csh" + "\n")
-                f.write("~dina/modeller9.18/bin/modpy.sh python3 " + RESTRAINTS_PATH + " " + os.getcwd() + " " + NANO_NET_MODEL_PATH)
-            subprocess.run("sbatch " + script_name, shell=True)  # sends script to the cluster
-            os.chdir("..")
+        loop_restraints()
 
     # for rosetta modeling (no loop modeling)
     if MODE == 'rosetta_model':
-        for pdb_dir in os.listdir(os.getcwd()):
-            if pdb_dir not in FAILED:
-                continue
-                # -detect_disulf false
-                # -camelid true
-            os.chdir(pdb_dir)
-            script_name = pdb_dir + ".sh"  # script file
-            with open(script_name, 'w') as f:
-                f.write(INTRO)
-                f.write("cd " + os.getcwd() + "\n")
-                f.write("setenv ROSETTA /cs/labs/dina/tomer.cohen13/Rosetta\n")
-                f.write("setenv ROSETTA3_DB $ROSETTA/main/database\n")
-                f.write("setenv ROSETTA_BIN $ROSETTA/main/source/bin\n")
-                f.write("setenv PATH $PATH':'$ROSETTA_BIN\n")
-                f.write("setenv PATH $PATH':'/cs/labs/dina/tomer.cohen13/Blast/bin\n")
-                f.write("antibody.linuxgccrelease -exclude_homologs true -vhh_only  -fasta " + pdb_dir+".fa -antibody:h1_template 1rzi | tee grafting.log\n")
-                f.write("cd grafting\n")
-                f.write("rm -f debug*\n")
-                f.write("rm -f orientation*\n")
-                f.write("rm -f frh*.pdb*\n")
-                f.write("rm -f frl*\n")
-                f.write("rm -f l*\n")
-            subprocess.run("sbatch " + script_name, shell=True)  # sends script to the cluster
-            os.chdir("..")
+        rosetta_model()
 
+    # for rosetta cdr3 loop modeling
     if MODE == 'rosetta_loops':
-        for pdb_dir in os.listdir(os.getcwd()):
-            if pdb_dir in FAILED:
-                continue
-                # -detect_disulf false
-                # -camelid true
-            os.chdir(pdb_dir)
-            script_name = pdb_dir + ".sh"  # script file
-            with open(script_name, 'w') as f:
-                f.write(INTRO)
-                f.write("cd " + os.getcwd() + "\n")
-                f.write("setenv ROSETTA /cs/labs/dina/tomer.cohen13/Rosetta\n")
-                f.write("setenv ROSETTA3_DB $ROSETTA/main/database\n")
-                f.write("setenv ROSETTA_BIN $ROSETTA/main/source/bin\n")
-                f.write("setenv PATH $PATH':'$ROSETTA_BIN\n")
-                f.write("setenv PATH $PATH':'/cs/labs/dina/tomer.cohen13/Blast/bin\n")
-                f.write("antibody_H3.linuxgccrelease @abH3.flags -s grafting/model-0.relaxed.pdb -nstruct 200 -out:file:scorefile H3_modeling_scores.fasc -out:path:pdb H3_modeling > h3_modeling-0.log\n")
-                # for model in range(1,10):
-                #     f.write("antibody_H3.linuxgccrelease @abH3.flags -s grafting/model-{}.relaxed.pdb -nstruct 100 > h3_modeling-{}.log\n".format(model, model))
-            subprocess.run("sbatch " + script_name, shell=True)  # sends script to the cluster
-            os.chdir("..")
+        rosetta_loops()
 
 
---
+++ nano_buddies/RosettaRMSD.py
@@ -0,0 +1,85 @@
+
+import argparse
+import numpy as np
+from NanoNetUtils import remove_pad, get_sequence
+import cdr_annotation
+
+import subprocess
+import os,sys,getopt
+
+
+rmsd_prog = "/cs/staff/dina/utils/rmsd"
+get_pdb = "/cs/staff/dina/scripts/getPDB.pl"
+get_pdb_chains = "/cs/staff/dina/scripts/getPDBChains.pl"
+renumber = "/cs/staff/dina/utils/srcs/renumber/renumber"
+rmsd_align = "/cs/staff/dina/scripts/alignRMSD.pl"
+get_frag_chain = "/cs/staff/dina/utils/get_frag_chain.Linux"
+
+N_LOOPS = 200
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pdb", help="pdb_dir")
+    args = parser.parse_args()
+
+    os.chdir(args.pdb)
+
+    model_seq = get_sequence(os.path.basename(args.pdb) + ".fa")
+
+    [cdr1_start, cdr1_end] = cdr_annotation.find_cdr1(model_seq)  # TODO- fix the cdr1 bug
+    [cdr3_start, cdr3_end] = cdr_annotation.find_cdr3(model_seq)
+    [cdr2_start, cdr2_end] = cdr_annotation.find_cdr2(model_seq)
+
+    # renumber for calculating cdrs rmsd (same length of models...)
+    subprocess.run(renumber + " ref.pdb > ref_renumber.pdb", shell=True)
+
+    all_rmsds = []
+    cdr1_rmsds = []
+    cdr2_rmsds = []
+    cdr3_rmsds = []
+
+    # score loops
+    for i in range(1, N_LOOPS+1):  # change to range(1, (loop_model_num*2)+1) for loop modeling 2
+        # read model file
+        code = "model-0.relaxed_%04d.pdb" % i
+        path = os.path.join("H3_modeling", code)
+        cmd = rmsd_prog + " -t ref.pdb " + code + " | tail -n1 "
+        rmsd_out = subprocess.check_output(cmd, shell=True)
+        rmsd = float(rmsd_out.strip())
+
+        # cdr 1,2,3 rmsd
+        subprocess.run(rmsd_align + " ref.pdb " + code, shell=True)  # align to get rmsd of cdr without cheating...
+        subprocess.run(get_frag_chain + " " + code.replace(".pdb", "_tr.pdb") + " ' ' " + str(cdr1_start) + " " + str(cdr1_end) + " > temp_cdr1.pdb", shell=True)
+        subprocess.run(get_frag_chain + " " + code.replace(".pdb", "_tr.pdb") + " ' ' " + str(cdr2_start) + " " + str(cdr2_end) + " > temp_cdr2.pdb", shell=True)
+        subprocess.run(get_frag_chain + " " + code.replace(".pdb", "_tr.pdb") + " ' ' " + str(cdr3_start) + " " + str(cdr3_end) + " > temp_cdr3.pdb", shell=True)
+
+        cdr1_rmsd = float(subprocess.check_output(rmsd_prog + " ref_cdr1.pdb temp_cdr1.pdb | tail -n1 ", shell=True).strip())
+        cdr2_rmsd = float(subprocess.check_output(rmsd_prog + " ref_cdr2.pdb temp_cdr2.pdb | tail -n1 ", shell=True).strip())
+        cdr3_rmsd = float(subprocess.check_output(rmsd_prog + " ref_cdr3.pdb temp_cdr3.pdb | tail -n1 ", shell=True).strip())
+
+        all_rmsds.append(rmsd)
+        cdr1_rmsds.append(cdr1_rmsd)
+        cdr2_rmsds.append(cdr2_rmsd)
+        cdr3_rmsds.append(cdr3_rmsd)
+
+        os.remove(code.replace(".pdb", "_tr.pdb"))
+        os.remove("temp_cdr1.pdb")
+        os.remove("temp_cdr2.pdb")
+        os.remove("temp_cdr3.pdb")
+
+    rosetta_scores = pd.read_csv("H3_modeling_scores.fasc", skiprows=1, delim_whitespace=True)
+    rosetta_scores.drop([0,1]) # first two loops missing cause running rosetta parallel
+
+    rosetta_scores["rmsd"] = all_rmsds
+    rosetta_scores["cdr1_rmsd"] = cdr1_rmsds
+    rosetta_scores["cdr2_rmsd"] = cdr2_rmsds
+    rosetta_scores["cdr3_rmsd"] = cdr3_rmsds
+
+    rosetta_scores.to_csv("H3_modeling_scores_rmsd.csv", index=False)
+
+
+
+
+    print("ended successfully")
