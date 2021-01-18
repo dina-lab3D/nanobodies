f35db6f37946d54f6f9b5cf1b11ce050ac3db50a
tomer.cohen13 <tomer.cohen13@mail.huji.ac.il>
2021-01-19 01:39:26 +0200

    fixed bugs from rosetta modeling differnces in seq from fasta, added *10 in distance

--- nano_buddies/RosettaConst.py
+++ nano_buddies/RosettaConst.py
@@ -2,15 +2,17 @@ from tensorflow.keras.models import load_model
 from NanoNetUtils import generate_input
 import argparse
 import numpy as np
-from NanoNetUtils import remove_pad, get_sequence
+from NanoNetUtils import remove_pad, get_sequence, get_seq
 import cdr_annotation
 import os
-
+from Bio.PDB import *
 
 DIST_STD = 0.63
 OMEGA_STD = 0.424
 THETA_STD = 0.3
 PHI_STD = 0.22
+
+PROBLEM = ["1XGR_1"]
 
 
 def write_const_dist(const_file, constraints, cdr3_s, seq):
@@ -24,7 +26,7 @@ def write_const_dist(const_file, constraints, cdr3_s, seq):
             atom_j_type = 'CA' if seq[atom_j] == 'G' else 'CB'  # GLY
             atom_i += 1  # pdb numbering starts from 1
             atom_j += 1  # pdb numbering starts from 1
-            const_file.write("AtomPair {} {} {} {} HARMONIC {} {}\n".format(atom_i_type, atom_i, atom_j_type, atom_j, constraints[i,j], DIST_STD))
+            const_file.write("AtomPair {} {} {} {} HARMONIC {:.5f} {}\n".format(atom_i_type, atom_i, atom_j_type, atom_j, constraints[i,j], DIST_STD))
 
 
 def write_const_omega(const_file, constraints, cdr3_s, seq):
@@ -38,7 +40,7 @@ def write_const_omega(const_file, constraints, cdr3_s, seq):
                 continue
             atom_i += 1  # pdb numbering starts from 1
             atom_j += 1  # pdb numbering starts from 1
-            const_file.write("Dihedral CA {} CB {} CB {} CA {} CIRCULARHARMONIC {} {}\n".format(atom_i, atom_i, atom_j, atom_j, constraints[i, j], OMEGA_STD))
+            const_file.write("Dihedral CA {} CB {} CB {} CA {} CIRCULARHARMONIC {:.5f} {}\n".format(atom_i, atom_i, atom_j, atom_j, constraints[i, j], OMEGA_STD))
 
 
 def write_const_theta(const_file, constraints, cdr3_s, seq):
@@ -54,8 +56,7 @@ def write_const_theta(const_file, constraints, cdr3_s, seq):
                 continue
             atom_i += 1  # pdb numbering starts from 1
             atom_j += 1  # pdb numbering starts from 1
-            const_file.write(
-                "Dihedral N {} CA {} CB {} CB {} CIRCULARHARMONIC {} {}\n".format(atom_i, atom_i, atom_i, atom_j, constraints[i, j], THETA_STD))
+            const_file.write("Dihedral N {} CA {} CB {} CB {} CIRCULARHARMONIC {:.5f} {}\n".format(atom_i, atom_i, atom_i, atom_j, constraints[i, j], THETA_STD))
 
 
 def write_const_phi(const_file, constraints, cdr3_s, seq):
@@ -70,42 +71,57 @@ def write_const_phi(const_file, constraints, cdr3_s, seq):
                 continue
             atom_i += 1  # pdb numbering starts from 1
             atom_j += 1  # pdb numbering starts from 1
-            const_file.write("Dihedral CA {} CB {} CB {} CIRCULARHARMONIC {} {}\n".format(atom_i, atom_i, atom_j, constraints[i, j], PHI_STD))
+            const_file.write("Angle CA {} CB {} CB {} CIRCULARHARMONIC {:.5f} {}\n".format(atom_i, atom_i, atom_j, constraints[i, j], PHI_STD))
 
 
-def sanity_check(distance_restraints, cdr3_s, cdr3_e):
+def write_const_file(sequence, restraints_matrix):
 
-    print(distance_restraints.shape)
+    distance_restraints = remove_pad(restraints_matrix[0][0,:,:,0], sequence) * 10  # we divided by factor 10 in NanoNet
+    omega_restraints = np.arctan2(remove_pad(restraints_matrix[1][0, :, :, 1], sequence), remove_pad(restraints_matrix[1][0, :, :, 0], sequence))  # angle = arctan(sin, cos)
+    thetha_restraints = np.arctan2(remove_pad(restraints_matrix[2][0, :, :, 1], sequence), remove_pad(restraints_matrix[2][0, :, :, 0], sequence))
+    phis_restraints = np.arctan2(remove_pad(restraints_matrix[3][0, :, :, 1], sequence), remove_pad(restraints_matrix[3][0, :, :, 0], sequence))
+
+    cdr_s, cdr_e = cdr_annotation.find_cdr3(sequence)
+    # print(cdr_e - cdr_s+1)
+    with open(pdb_dir + "_constraints", 'w') as const_file:
+        write_const_dist(const_file, distance_restraints, cdr_s, sequence)
+        write_const_omega(const_file, omega_restraints, cdr_s, sequence)
+        write_const_theta(const_file, thetha_restraints, cdr_s, sequence)
+        write_const_phi(const_file, phis_restraints, cdr_s, sequence)
+
+
+def sanity_check(all_restraints, pdb, sequence):
+
+    pdb_model = PDBParser().get_structure(pdb, "grafting/model-0.relaxed.pdb")[0]["H"]
+    seq_fa = get_sequence(pdb + ".fa")
+    seq_aa, _ = get_seq(pdb_model)
+    cdr3_s, cdr3_e = cdr_annotation.find_cdr3(sequence)
+    distance_restraints = remove_pad(all_restraints[0][0, :, :, 0], sequence) * 10
+    error = False
+
     if (cdr3_e - cdr3_s + 1) != distance_restraints.shape[0] or distance_restraints.shape[0] != distance_restraints.shape[1]:
         print(cdr3_e - cdr3_s + 1)
         print(distance_restraints.shape[0])
         print(distance_restraints.shape[1])
         print("cdr3 sequence error!!!")
-        exit(2)
+        error = True
 
     if np.sum(distance_restraints[0]) == 0 or np.sum(distance_restraints[-1]) == 0 \
             or np.sum(distance_restraints[:,0]) ==0 or np.sum(distance_restraints[:,-1]) == 0:
         print("remove pad error!!!")
-        exit(3)
+        error = True
 
+    a, b = cdr_annotation.find_cdr3(seq_aa)
+    c, d = cdr_annotation.find_cdr3(seq_fa)
 
-def write_const_file(fasta_file, restraints_matrix):
-
-    seq = get_sequence(fasta_file)
-
-    distance_restraints = remove_pad(restraints_matrix[0][0,:,:,0], seq)
-    omega_restraints = np.arctan2(remove_pad(restraints_matrix[1][0, :, :, 1], seq), remove_pad(restraints_matrix[1][0, :, :, 0], seq))  # angle = arctan(sin, cos)
-    thetha_restraints = np.arctan2(remove_pad(restraints_matrix[2][0, :, :, 1], seq), remove_pad(restraints_matrix[2][0, :, :, 0], seq))
-    phis_restraints = np.arctan2(remove_pad(restraints_matrix[3][0, :, :, 1], seq), remove_pad(restraints_matrix[3][0, :, :, 0], seq))
-
-    cdr_s, cdr_e = cdr_annotation.find_cdr3(seq)
-    sanity_check(distance_restraints, cdr_s, cdr_e)
-
-    with open(pdb_dir + "constraints", 'w') as const_file:
-        write_const_dist(const_file, distance_restraints, cdr_s, seq)
-        write_const_omega(const_file, omega_restraints, cdr_s, seq)
-        write_const_theta(const_file, thetha_restraints, cdr_s, seq)
-        write_const_phi(const_file, phis_restraints, cdr_s, seq)
+    if seq_aa[a:b+1] != seq_fa[c:d+1]:
+        print(seq_aa[a:b+1])
+        print(seq_fa[c:d+1])
+        print(sequence[cdr3_s:cdr3_e+1])
+        print("find_cdr3 error!!!")
+        error = True
+    if not error:
+        print("{} finished successfully!".format(pdb))
 
 
 if __name__ == '__main__':
@@ -119,10 +135,19 @@ if __name__ == '__main__':
     os.chdir(args.Rosetta_pdbs)
 
     for pdb_dir in os.listdir(os.getcwd()):
-        os.chdir(pdb_dir)
-        pdb_fasta = pdb_dir + ".fa"
-        restraints = nano_net_model.predict(np.array([generate_input(pdb_fasta)]))
-        write_const_file(pdb_fasta, restraints)
-        os.chdir("..")
+        if os.path.isdir(pdb_dir) and pdb_dir != "1YC7_1":
+            os.chdir(pdb_dir)
+            model = PDBParser().get_structure(pdb_dir, "grafting/model-0.relaxed.pdb")[0]["H"]
+            if pdb_dir in PROBLEM:
+                seq = get_sequence(pdb_dir + ".fa")
+                print(pdb_dir)
+            else:
+                seq, aa_chain = get_seq(model)
+
+            restraints = nano_net_model.predict(np.array([generate_input(seq, fasta=False)]))
+            write_const_file(seq, restraints)
+            sanity_check(restraints, pdb_dir, seq)
+            os.chdir("..")
+
 
 
