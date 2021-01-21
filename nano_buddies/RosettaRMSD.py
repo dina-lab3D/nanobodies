faa9ab31e026be0ce61a8da37ad21accefc59684
tomer.cohen13 <tomer.cohen13@mail.huji.ac.il>
2021-01-21 19:39:37 +0200

    - solved problems with different length in rosetta output...

--- nano_buddies/RosettaRMSD.py
+++ nano_buddies/RosettaRMSD.py
@@ -17,24 +17,74 @@ get_frag_chain = "/cs/staff/dina/utils/get_frag_chain.Linux"
 
 N_LOOPS = 200
 PROBLEM = ["1XGR_1"]
+MATCH_SUB = []
+MATCH_FULL = []
+MATCH_CUT = []
+SEQS = []
 
 
-def calc_rmsds(pdb_dir):
+def adjust_lengths(ref_seq, model_seq, pdb_dir):
+
+    subprocess.run(renumber + " ref.pdb > ref_renumber.pdb", shell=True, stdout=subprocess.DEVNULL)
+    match = re.search(model_seq, ref_seq)
+    indexes = None
+
+    if match:
+        start, end = match.span()
+        subprocess.run(get_frag_chain + " ref_renumber.pdb H " + str(start+1) + " " + str(end) + " > temp.pdb", shell=True, stdout=subprocess.DEVNULL)
+        subprocess.run(renumber + " temp.pdb > ref_same_size.pdb", shell=True, stdout=subprocess.DEVNULL)
+        os.remove("temp.pdb")
+
+    else:
+        match = re.search(ref_seq, model_seq)
+        if match:
+            subprocess.run("cp ref_renumber.pdb ref_same_size.pdb", shell=True, stdout=subprocess.DEVNULL)
+            indexes = match.span()
+
+            MATCH_FULL.append(pdb_dir)
+        else:
+            match = re.search(ref_seq[:-1], model_seq)
+            if match:
+                subprocess.run(get_frag_chain + " ref_renumber.pdb H " + str(1) + " " + str(len(ref_seq) - 1) + " > ref_same_size.pdb", shell=True, stdout=subprocess.DEVNULL)
+                indexes = match.span()
+
+                MATCH_SUB.append(pdb_dir)
+            else:
+                i = 0
+                while not match:
+                    i += 1
+                    match = re.search(model_seq[i:], ref_seq)
+                    if i == "50":
+                        print(pdb_dir)
+                        exit(1)
+
+                start, end = match.span()
+                subprocess.run(get_frag_chain + " ref_renumber.pdb H " + str(start+1) + " " + str(end) + " > temp.pdb", shell=True, stdout=subprocess.DEVNULL)
+                subprocess.run(renumber + " temp.pdb > ref_same_size.pdb", shell=True, stdout=subprocess.DEVNULL)
+                os.remove("temp.pdb")
+                indexes = (i, len(model_seq))
+
+                MATCH_CUT.append(pdb_dir)
+                SEQS.append([ref_seq[start:end], model_seq[i:]])
+
+    os.remove("ref_renumber.pdb")
+    return indexes
+
+
+def calc_rmsds(pdb_dir, nano_net):
 
     os.chdir(pdb_dir)
-    model = PDBParser().get_structure("grafting/model-0.relaxed.pdb", "grafting/model-0.relaxed.pdb")[0]["H"]
+
+    model = PDBParser().get_structure("H3_modeling/model-0.relaxed_0001.pdb", "H3_modeling/model-0.relaxed_0001.pdb")[0]["H"]
     model_seq, _ = get_seq(model)
 
     ref_model = PDBParser().get_structure("ref.pdb", "ref.pdb")[0]["H"]
     ref_seq, _ = get_seq(ref_model)
-
-    start, end = re.search(model_seq, ref_seq).span()
-    subprocess.run(get_frag_chain + " ref.pdb H " + str(start) + " " + str(end) + " > ref_same_size.pdb", shell=True)
+    indexes = adjust_lengths(ref_seq, model_seq, pdb_dir)
 
     [cdr1_start, cdr1_end] = cdr_annotation.find_cdr1(model_seq)
     [cdr2_start, cdr2_end] = cdr_annotation.find_cdr2(model_seq)
     [cdr3_start, cdr3_end] = cdr_annotation.find_cdr3(model_seq)
-
     if pdb_dir in PROBLEM:
         correct_seq = get_sequence(pdb_dir + ".fa")
         [cdr3_start, cdr3_end] = cdr_annotation.find_cdr3(correct_seq)
@@ -43,9 +93,17 @@ def calc_rmsds(pdb_dir):
         print(cdr_annotation.find_cdr3(model_seq))
         print(cdr_annotation.find_cdr3(correct_seq))
 
-    subprocess.run(get_frag_chain + " ref_same_size.pdb H " + str(cdr1_start) + " " + str(cdr1_end) + " > ref_cdr1.pdb", shell=True)
-    subprocess.run(get_frag_chain + " ref_same_size.pdb H " + str(cdr2_start) + " " + str(cdr2_end) + " > ref_cdr2.pdb", shell=True)
-    subprocess.run(get_frag_chain + " ref_same_size.pdb H " + str(cdr3_start) + " " + str(cdr3_end) + " > ref_cdr3.pdb", shell=True)
+    if indexes:
+        cdr1_start -= indexes[0]
+        cdr2_start -= indexes[0]
+        cdr3_start -= indexes[0]
+        cdr1_end -= indexes[0]
+        cdr2_end -= indexes[0]
+        cdr3_end -= indexes[0]
+
+    subprocess.run(get_frag_chain + " ref_same_size.pdb H " + str(cdr1_start+1) + " " + str(cdr1_end) + " > ref_cdr1.pdb", shell=True, stdout=subprocess.DEVNULL)
+    subprocess.run(get_frag_chain + " ref_same_size.pdb H " + str(cdr2_start+1) + " " + str(cdr2_end) + " > ref_cdr2.pdb", shell=True, stdout=subprocess.DEVNULL)
+    subprocess.run(get_frag_chain + " ref_same_size.pdb H " + str(cdr3_start+1) + " " + str(cdr3_end) + " > ref_cdr3.pdb", shell=True, stdout=subprocess.DEVNULL)
 
     all_rmsds = []
     cdr1_rmsds = []
@@ -53,46 +111,63 @@ def calc_rmsds(pdb_dir):
     cdr3_rmsds = []
     cdr3_lengths = []
 
+    if nano_net:
+        folder = "H3_NanoNet_modeling"
+        score_file = "H3_NanoNet_modeling_scores.fasc"
+        rmsd_file = "H3_NanoNet_modeling_scores_rmsd.csv"
+
+    else:
+        folder = "H3_modeling"
+        score_file = "H3_modeling_scores.fasc"
+        rmsd_file = "H3_modeling_scores_rmsd.csv"
+
     # score loops
     for i in range(1, N_LOOPS+1):  # change to range(1, (loop_model_num*2)+1) for loop modeling 2
         # read model file
         code = "model-0.relaxed_%04d.pdb" % i
-        model_path = os.path.join("H3_modeling", code)
-        cmd = rmsd_prog + " -t ref_same_size.pdb " + model_path + " | tail -n1 "
-        rmsd_out = subprocess.check_output(cmd, shell=True)
-        rmsd = float(rmsd_out.strip())
+        model_path = os.path.join(folder, code)
+        subprocess.run(renumber + " {} > model_renumber.pdb".format(model_path), shell=True, stdout=subprocess.DEVNULL)
+        if indexes:
+            subprocess.run(get_frag_chain + " model_renumber.pdb H " + str(indexes[0]+1) + " " + str(indexes[1]) + " > temp.pdb", shell=True, stdout=subprocess.DEVNULL)
+            os.remove("model_renumber.pdb")
+            subprocess.run(renumber + " temp.pdb > model_renumber.pdb", shell=True, stdout=subprocess.DEVNULL)
+            os.remove("temp.pdb")
+
+        cmd = rmsd_prog + " -t ref_same_size.pdb model_renumber.pdb | tail -n1 "
+        rmsd = float(subprocess.run(cmd, shell=True, capture_output=True).stdout.strip())
 
         # cdr 1,2,3 rmsd
-        subprocess.run(rmsd_align + " ref_same_size.pdb " + model_path, shell=True)  # align to get rmsd of cdr without cheating...
-        subprocess.run(get_frag_chain + " " + model_path.replace(".pdb", "_tr.pdb") + " H " + str(cdr1_start) + " " + str(cdr1_end) + " > temp_cdr1.pdb", shell=True)
-        subprocess.run(get_frag_chain + " " + model_path.replace(".pdb", "_tr.pdb") + " H " + str(cdr2_start) + " " + str(cdr2_end) + " > temp_cdr2.pdb", shell=True)
-        subprocess.run(get_frag_chain + " " + model_path.replace(".pdb", "_tr.pdb") + " H " + str(cdr3_start) + " " + str(cdr3_end) + " > temp_cdr3.pdb", shell=True)
+        subprocess.run(rmsd_align + " ref_same_size.pdb model_renumber.pdb", shell=True, stdout=subprocess.DEVNULL)  # align to get rmsd of cdr without cheating...
+        subprocess.run(get_frag_chain + " model_renumber_tr.pdb H " + str(cdr1_start+1) + " " + str(cdr1_end) + " > temp_cdr1.pdb", shell=True, stdout=subprocess.DEVNULL)
+        subprocess.run(get_frag_chain + " model_renumber_tr.pdb H " + str(cdr2_start+1) + " " + str(cdr2_end) + " > temp_cdr2.pdb", shell=True, stdout=subprocess.DEVNULL)
+        subprocess.run(get_frag_chain + " model_renumber_tr.pdb H " + str(cdr3_start+1) + " " + str(cdr3_end) + " > temp_cdr3.pdb", shell=True, stdout=subprocess.DEVNULL)
 
-        cdr1_rmsd = float(subprocess.check_output(rmsd_prog + " ref_cdr1.pdb temp_cdr1.pdb | tail -n1 ", shell=True).strip())
-        cdr2_rmsd = float(subprocess.check_output(rmsd_prog + " ref_cdr2.pdb temp_cdr2.pdb | tail -n1 ", shell=True).strip())
-        cdr3_rmsd = float(subprocess.check_output(rmsd_prog + " ref_cdr3.pdb temp_cdr3.pdb | tail -n1 ", shell=True).strip())
+        cdr1_rmsd = float(subprocess.run(rmsd_prog + " ref_cdr1.pdb temp_cdr1.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
+        cdr2_rmsd = float(subprocess.run(rmsd_prog + " ref_cdr2.pdb temp_cdr2.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
+        cdr3_rmsd = float(subprocess.run(rmsd_prog + " ref_cdr3.pdb temp_cdr3.pdb | tail -n1 ", shell=True, capture_output=True).stdout.strip())
 
         all_rmsds.append(rmsd)
         cdr1_rmsds.append(cdr1_rmsd)
         cdr2_rmsds.append(cdr2_rmsd)
         cdr3_rmsds.append(cdr3_rmsd)
         cdr3_lengths.append(cdr3_end - cdr3_start)
-        os.remove(model_path.replace(".pdb", "_tr.pdb"))
+        os.remove("model_renumber.pdb")
+        os.remove("model_renumber_tr.pdb")
         os.remove("temp_cdr1.pdb")
         os.remove("temp_cdr2.pdb")
         os.remove("temp_cdr3.pdb")
 
-    rosetta_scores = pd.read_csv("H3_modeling_scores.fasc", skiprows=1, delim_whitespace=True)
+    rosetta_scores = pd.read_csv(score_file, skiprows=1, delim_whitespace=True)
     rosetta_scores.drop_duplicates(subset="description", keep="last", inplace=True) # when running rosetta in parallel it can cause several models with same name
 
     rosetta_scores["rmsd"] = all_rmsds
     rosetta_scores["cdr1_rmsd"] = cdr1_rmsds
     rosetta_scores["cdr2_rmsd"] = cdr2_rmsds
     rosetta_scores["cdr3_rmsd"] = cdr3_rmsds
-    rosetta_scores["cdr3_length"] = cdr3_lengths
+    rosetta_scores["length"] = cdr3_lengths
     rosetta_scores.rename(columns={"description":"name"}, inplace=True)
-    rosetta_scores["type"] = ["ROSETTA_LOOP"] * len(rosetta_scores["rmsd"])
-    rosetta_scores.to_csv("H3_modeling_scores_rmsd.csv", index=False, columns=["type", "name", "total_score", "unconstr_score", "score", "rmsd", "cdr1_rmsd", "cdr2_rmsd", "cdr3_rmsd", "cdr3_length"])
+    rosetta_scores["type"] = ["LOOP"] * len(rosetta_scores["rmsd"])
+    rosetta_scores.to_csv(rmsd_file, index=False, columns=["type", "name", "total_score", "unconstr_score", "score", "rmsd", "cdr1_rmsd", "cdr2_rmsd", "cdr3_rmsd", "length"])
 
     os.chdir("..")
 
@@ -101,12 +176,36 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("pdbs", help="pdsb_dir")
+    parser.add_argument("-n", "--nano_net", help="use results after using restraints with NanoNet", action="store_true")
     args = parser.parse_args()
 
+    k = 1
     os.chdir(args.pdbs)
     for pdb in os.listdir(os.getcwd()):
-        if os.path.exists(os.path.join(pdb, "H3_modeling_scores.fasc")):
-            calc_rmsds(pdb)
-            print("{} ended successfully".format(pdb))
-        else:
-            print("{} Failed, no score file".format(pdb))
+
+        # if pdb != "2HWZ_1":
+        #     continue
+        os.chdir(pdb)
+        subprocess.run("rm -f ref_cdr*", shell=True)
+        subprocess.run("rm -f H3_modeling_scores_rmsd.csv", shell=True)
+        subprocess.run("rm -f ref_same_size.pdb", shell=True)
+        subprocess.run("rm -f model_renumber.pdb", shell=True)
+        subprocess.run("rm -f temp.pdb", shell=True)
+        subprocess.run("rm -f temp_cdr1.pdb", shell=True)
+        subprocess.run("rm -f temp_cdr2.pdb", shell=True)
+        subprocess.run("rm -f temp_cdr3.pdb", shell=True)
+
+        os.chdir("..")
+        # if os.path.exists(os.path.join(pdb, "H3_modeling_scores.fasc")):
+        #     print(pdb)
+        #     calc_rmsds(pdb, args.nano_net)
+        #     print("{} ended successfully".format(pdb))
+        # else:
+        #     print("{} Failed, no score file".format(pdb))
+        # print("NUM: " + str(k))
+        # k += 1
+
+    print(MATCH_FULL)
+    print(MATCH_SUB)
+    print(MATCH_CUT)
+    print(SEQS)
